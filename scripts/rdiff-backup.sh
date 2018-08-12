857727f753250ff5b8237156bc7d0b01614cb2d3
dissonmatveev <disson@yandex.ru>
2018-08-12 22:57:34 +0500

    verifying backups added

--- scripts/rdiff-backup.sh
+++ scripts/rdiff-backup.sh
@@ -7,11 +7,12 @@ print_help()
   echo -e "Usage: rdiff-backup.sh -d dirname [OPTION]"
   echo -e "\t-d specifies path to dir with rdiff-backup"
   echo -e "OPTIONS are:"
-  echo -e "\t-s prints size in bytes of rdiff-backup"
-  echo -e "\t-t prints duration of the last backup"
-  echo -e "\t-c prints growth size in bytes of the last backup"
+  echo -e "\t-s prints size of rdiff-backup"
+  echo -e "\t-t prints elapsed time"
+  echo -e "\t-c prints size cache of backup"
   echo -e "\t-r prints start time of backup"
-  echo -e "\t-g prints timestamp when the last backup was started"
+  echo -e "\t-g prints start time of backup"
+  echo -e "\t-v verify the last backups and prints 0 of OK and non-zero if no"
 }
 
 
@@ -19,7 +20,7 @@ if [ $# -eq 0 ];then
   find ${BACKUPDIR}  -maxdepth 3 -type d ! -readable -prune -o -name rdiff-backup-data -exec dirname {} \; | awk 'BEGIN{printf "{\"data\":["}; {i=split($1,path,"/"); printf c"{\"{#PATH}\":\""$1"\", \"{#HOST}\":\"" path[i-1] "\", \"{#DIR}\":\"" path[i] "\"}";c="," };  END{print "]}"}'
 fi
 
-while getopts "d:stcrgh" opt
+while getopts "d:stcrghv" opt
 do
   case ${opt} in
   d) DIR=${OPTARG}
@@ -39,6 +40,9 @@ do
   g) grep StartTime ${DIR}/rdiff-backup-data/session_statistics.$(basename $(ls ${DIR}/rdiff-backup-data/current_mirror.*.data) | cut -d "." -f2).data | sed 's/^.*(//;s/)$//'
   break
   ;; 
+  v) /usr/bin/rdiff-backup --verify ${DIR} >/dev/null; echo $?
+  break
+  ;;
   h) print_help
   exit 0
   ;;
@@ -47,9 +51,3 @@ do
   ;;
   esac
 done
-
-
-
-
-
-
